4c48a0487db8313ea1b3ec5fd84ae179eb2b5212
NArun412 <arunmozhivarathan.n@mahathiinfotech.com>
2024-04-02 15:40:27 +0530

    git ignore added

--- public/db_constants.php
+++ public/db_constants.php
@@ -2,6 +2,6 @@
 	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','localhost');
 	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','root');
 	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','');
-	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','sentrifugo_hr');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','sentrifugo');
 	           
 	         ?>
